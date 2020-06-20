089994f0d450e08b6f987e2511903766ca14514e
NATARAJAN @. KUMAR N C KRISHNAN <rajan@my.ibm.com>
2020-06-21 02:27:36 +0800

    Update scenario1.tf

--- scenario1.tf
+++ scenario1.tf
@@ -17,12 +17,12 @@ variable "hostname" {
   default     = "debian-small"
 }
 
-## This will create a new SSH key that will show up under the \
+# This will create a new SSH key that will show up under the \
 # Devices>Manage>SSH Keys in the SoftLayer console.
-#resource "ibm_compute_ssh_key" "orpheus_public_key" {
-#  label      = "Orpheus Public Key"
-#  public_key = "${var.public_ssh_key}"
-#}
+resource "ibm_compute_ssh_key" "orpheus_public_key" {
+  label      = "Orpheus Public Key"
+  public_key = "${var.public_ssh_key}"
+}
 
 variable "domain" {
   description = "VM domain"
@@ -31,16 +31,19 @@ variable "domain" {
 # Create a new virtual guest using image "Debian"
 resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
   hostname                 = "${var.hostname}"
-  os_reference_code        = "DEBIAN_8_64"
+  os_reference_code        = "CENTOS_7_64"
   domain                   = "${var.domain}"
   datacenter               = "${var.datacenter}"
-  network_speed            = 100
+  network_speed            = 10
   hourly_billing           = true
   private_network_only     = false
-  cores                    = 1
-  memory                   = 1024
+  cores                    = 2
+  memory                   = 4096
   disks                    = [25]
   user_metadata            = "{\"value\":\"newvalue\"}"
+  dedicated_acct_host_only = false
+  local_disk               = false
+  ssh_key_ids              = ["${ibm_compute_ssh_key.orpheus_public_key.id}"]
   tags                     = ["${module.camtags.tagslist}"]
 }
 
